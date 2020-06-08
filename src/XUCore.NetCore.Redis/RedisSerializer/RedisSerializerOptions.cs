cea479f5e4555050922ca6213dfafe6e8048b01d
Nigel <Nigel@DESKTOP-VVIB5CF>
2020-06-08 08:00:55 +0800

    增加序列化选项

---
+++ src/XUCore.NetCore.Redis/RedisSerializer/RedisSerializerOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUCore.NetCore.Redis
+{
+    public static class RedisSerializerOptions
+    {
+        public static IRedisSerializer Json
+        {
+            get { return new JsonRedisSerializer(); }
+        }
+        public static IRedisSerializer MessagePack
+        {
+            get { return new MessagePackRedisSerializer(); }
+        }
+        public static IRedisSerializer RedisValue
+        {
+            get { return new RedisValueSerializer(); }
+        }
+    }
+}
