e37d73530223012e02f415ab2b2b0ba701d4a85b
Nigel <Nigel@DESKTOP-VVIB5CF>
2020-06-30 15:04:22 +0800

    修改DataTable转换泛型集合

--- src/XUCore/Base/Extensions/Datas/DataTableExtensions.cs
+++ src/XUCore/Base/Extensions/Datas/DataTableExtensions.cs
@@ -16,36 +16,70 @@ namespace XUCore.Extensions.Datas
         /// </summary>
         /// <typeparam name="T">类型</typeparam>
         /// <param name="dataTable">数据表</param>
-        public static IList<T> ToList<T>(this DataTable dataTable)
+        public static IList<T> ToList<T>(this DataTable dataTable) where T : new()
         {
             if (dataTable == null)
                 throw new ArgumentNullException(nameof(dataTable), @"数据表不可为空！");
-            var type = typeof(T);
-            var properties = type.GetProperties();
-            var constructors =
-                type.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-            var noParamCtor = constructors.Single(x => x.GetParameters().Length == 0);
-            var collection = new List<T>();
-            foreach (DataRow row in dataTable.Rows)
+
+            var columnNames = dataTable.Columns.Cast<DataColumn>()
+                .Select(c => c.ColumnName)
+                .ToList();
+
+            var properties = typeof(T).GetProperties();
+
+            return dataTable.AsEnumerable().Select(row =>
             {
-                var instance = (T)noParamCtor.Invoke(null);
-                foreach (var property in properties)
+                T objT = new T();
+
+                foreach (var pro in properties)
                 {
-                    if (dataTable.Columns.Contains(property.Name))
+                    if (columnNames.Contains(pro.Name))
                     {
-                        var setter = property.GetSetMethod(true);
-                        if (setter != null)
-                        {
-                            var value = row[property.Name] == DBNull.Value ? null : row[property.Name];
-                            setter.Invoke(instance, new[] { value });
-                        }
+                        if (!pro.CanWrite) continue;
+                        var value = row[pro.Name] == DBNull.Value ? null : row[pro.Name];
+                        pro.SetValue(objT, Convert.ChangeType(value, pro.PropertyType), null);
                     }
                 }
 
-                collection.Add(instance);
-            }
+                return objT;
 
-            return collection;
+            }).ToList();
         }
+        ///// <summary>
+        ///// DataTable转换为泛型集合
+        ///// </summary>
+        ///// <typeparam name="T">类型</typeparam>
+        ///// <param name="dataTable">数据表</param>
+        //public static IList<T> ToList<T>(this DataTable dataTable)
+        //{
+        //    if (dataTable == null)
+        //        throw new ArgumentNullException(nameof(dataTable), @"数据表不可为空！");
+        //    var type = typeof(T);
+        //    var properties = type.GetProperties();
+        //    var constructors =
+        //        type.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        //    var noParamCtor = constructors.Single(x => x.GetParameters().Length == 0);
+        //    var collection = new List<T>();
+        //    foreach (DataRow row in dataTable.Rows)
+        //    {
+        //        var instance = (T)noParamCtor.Invoke(null);
+        //        foreach (var property in properties)
+        //        {
+        //            if (dataTable.Columns.Contains(property.Name))
+        //            {
+        //                var setter = property.GetSetMethod(true);
+        //                if (setter != null)
+        //                {
+        //                    var value = row[property.Name] == DBNull.Value ? null : row[property.Name];
+        //                    setter.Invoke(instance, new[] { value });
+        //                }
+        //            }
+        //        }
+
+        //        collection.Add(instance);
+        //    }
+
+        //    return collection;
+        //}
     }
 }
