ef559d4b764b649127b4c4025558ac600dee8131
Nigel <3624091@qq.com>
2020-07-15 11:59:40 +0800

    增加pagedlinq扩展分页

---
+++ src/XUCore.NetCore.Data/DbService/PagedExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XUCore.Extensions;
+using XUCore.Paging;
+
+namespace XUCore.NetCore.Data.DbService
+{
+    public static partial class PagedExtensions
+    {
+        public static PagedModel<T> CreatePagedList<T>(this IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            var count = query.LongCount();
+
+            var list = query.PageBy((pageNumber - 1) * pageSize, pageSize).ToList();
+
+            return new PagedModel<T>(list, count, pageNumber, pageSize);
+        }
+
+        public static async Task<PagedModel<T>> CreatePagedListAsync<T>(this IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            var count = await query.LongCountAsync();
+
+            var list = await query.PageBy((pageNumber - 1) * pageSize, pageSize).ToListAsync();
+
+            return new PagedModel<T>(list, count, pageNumber, pageSize);
+        }
+    }
+}
