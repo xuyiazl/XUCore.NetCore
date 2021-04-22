bd8c09c0b14f4fa651e997868187ab29be0ae0c2
Nigel <Nigel@PS-03>
2021-04-22 18:00:09 +0800

    测试事务AOP - demo

--- test/XUCore.NetCore.DataTest/TransactionAttribute.cs
+++ test/XUCore.NetCore.DataTest/TransactionAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using AspectCore.DynamicProxy;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -36,6 +37,45 @@ namespace XUCore.NetCore.DataTest
                     throw error;
                 },
                 CancellationToken.None);
+        }
+    }
+    /// <summary>
+    /// 测试事务AOP - demo
+    /// </summary>
+    public class Transaction1Attribute : InterceptorBase
+    {
+        /// <summary>
+        /// Type of DbContext
+        /// </summary>
+        public Type DbType { get; set; }
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            var dbContext = context.ServiceProvider.GetService(DbType) as IDbContext;
+
+            if (dbContext == null) throw new ArgumentNullException("DbType is null");
+
+            IUnitOfWork unitOfWork = new UnitOfWork(dbContext);
+
+            var strategy = unitOfWork.CreateExecutionStrategy();
+
+            await strategy.ExecuteAsync(async (_cancel) =>
+            {
+                using (var tran = await unitOfWork.BeginTransactionAsync(_cancel))
+                {
+                    try
+                    {
+                        await next(context);
+
+                        await tran.CommitAsync(_cancel);
+                    }
+                    catch (Exception ex)
+                    {
+                        await tran.RollbackAsync(_cancel);
+
+                        throw;
+                    }
+                }
+            }, CancellationToken.None);
         }
     }
 }
