51158d2f050b3cfc08c97b6d3c1e8cbe8a2e2e7d
Nigel <Nigel@PS-03>
2021-04-27 17:34:49 +0800

    增加主键mapping映射抽象类

---
+++ src/XUCore.NetCore.Data/DbService/KeyMapping.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using XUCore.Ddd.Domain;
+
+namespace XUCore.NetCore.Data.DbService
+{
+    public abstract class KeyMapping<T> : EntityTypeConfiguration<T>
+         where T : Entity, new()
+    {
+        public KeyMapping(string tableName, Expression<Func<T, object>> primaryKey) : base(tableName, primaryKey)
+        {
+            SetIndentity(t => t.Id);
+        }
+
+        public override void Configure(EntityTypeBuilder<T> builder)
+        {
+            base.Configure(builder);
+        }
+    }
+}
