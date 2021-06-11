98b9339a77c54902861fddbebb51c4bd76cb858a
Nigel <3624091@qq.com>
2021-06-11 17:29:59 +0800

    swagger新增jwt自动登录

--- src/XUCore.NetCore.Swagger/SwaggerExtensions.cs
+++ src/XUCore.NetCore.Swagger/SwaggerExtensions.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -124,6 +126,58 @@ namespace XUCore.NetCore.Swagger
             options.DocumentFilter<ControllerDescriptions>(apiXml);
             //添加隐藏API过滤器，可以在API上加HiddenApi标签隐藏。
             options.DocumentFilter<HiddenApiFilter>();
+        }
+
+        /// <summary>
+        /// 设置Swagger自动登录
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <param name="accessToken"></param>
+        public static void SigninToSwagger(this HttpContext httpContext, string accessToken)
+        {
+            // 设置 Swagger 刷新自动授权
+            httpContext.Response.Headers["access-token"] = accessToken;
+        }
+
+        /// <summary>
+        /// 设置Swagger自动登录
+        /// </summary>
+        /// <param name="httpContextAccessor"></param>
+        /// <param name="accessToken"></param>
+        public static void SigninToSwagger(this IHttpContextAccessor httpContextAccessor, string accessToken)
+        {
+            httpContextAccessor.HttpContext.SigninToSwagger(accessToken);
+        }
+
+        /// <summary>
+        /// 设置Swagger退出登录
+        /// </summary>
+        /// <param name="httpContext"></param>
+        public static void SignoutToSwagger(this HttpContext httpContext)
+        {
+            httpContext.Response.Headers["access-token"] = "invalid_token";
+        }
+
+        /// <summary>
+        /// 设置Swagger退出登录
+        /// </summary>
+        /// <param name="httpContextAccessor"></param>
+        public static void SignoutToSwagger(this IHttpContextAccessor httpContextAccessor)
+        {
+            httpContextAccessor.HttpContext.SignoutToSwagger();
+        }
+        /// <summary>
+        /// 注入 MiniProfiler 插件（自动登录）
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="injectMiniProfiler"></param>
+        public static void InjectMiniProfilerPlugin(this SwaggerUIOptions options, bool injectMiniProfiler = false)
+        {
+            var thisType = typeof(SwaggerExtensions);
+            var thisAssembly = thisType.Assembly;
+
+            // 自定义 Swagger 首页
+            options.IndexStream = () => thisAssembly.GetManifestResourceStream($"{thisType.Namespace}.Assets.{(injectMiniProfiler != true ? "index" : "index-mini-profiler")}.html");
         }
     }
 }
