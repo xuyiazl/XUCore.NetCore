945105f9a7385028446347ec9974a0fa60d930e5
Nigel <3624091@qq.com>
2021-07-27 16:31:22 +0800

    增加HttpContext上下文扩展-GetMetadata

---
+++ src/XUCore.NetCore/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUCore.NetCore
+{
+    public static class HttpContextExtensions
+    {
+        /// <summary>
+        /// 获取 Action 特性
+        /// </summary>
+        /// <typeparam name="TAttribute"></typeparam>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        public static TAttribute GetMetadata<TAttribute>(this HttpContext httpContext)
+            where TAttribute : class
+        {
+            return httpContext.GetEndpoint()?.Metadata?.GetMetadata<TAttribute>();
+        }
+    }
+}
