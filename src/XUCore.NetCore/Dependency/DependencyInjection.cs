09a6ccb454e0056ab560c278a0f12577b3eb63b9
Nigel <3624091@qq.com>
2021-09-30 09:38:45 +0800

    优化扫描生命周期

--- src/XUCore.NetCore/Dependency/DependencyInjection.cs
+++ src/XUCore.NetCore/Dependency/DependencyInjection.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyModel;
+using Scrutor;
+using System;
+using System.Reflection;
 
 namespace XUCore.NetCore
 {
@@ -9,15 +12,15 @@ namespace XUCore.NetCore
     public static partial class DependencyInjection
     {
         /// <summary>
-        /// 扫描注册生命周期对象
+        /// 指定扫描方式，扫描生命周期，效率较高。
         /// </summary>
         /// <param name="services"></param>
+        /// <param name="fromAssembly">当指定为null，则默认从DependencyContext中扫描</param>
         /// <returns></returns>
-        public static IServiceCollection AddScanLifetime(this IServiceCollection services)
+        public static IServiceCollection AddScanLifetime(this IServiceCollection services, Func<IAssemblySelector, IImplementationTypeSelector> fromAssembly = null)
         {
             services.Scan(scan =>
-                scan
-                    .FromDependencyContext(DependencyContext.Default)
+                    (fromAssembly == null ? scan.FromDependencyContext(DependencyContext.Default) : fromAssembly(scan))
                     //扫描单例
                     .AddClasses(impl => impl.AssignableTo<ISingleton>())
                     //.AsImplementedInterfaces()
