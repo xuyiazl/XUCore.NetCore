bd26cfdec9e14634fb9132a176bd3614a413ec2b
Nigel <3624091@qq.com>
2021-11-29 13:44:21 +0800

    1

--- push_packages.ps1
+++ push_packages.ps1
@@ -9,7 +9,7 @@ $version = $commonPropsXml.Project.PropertyGroup.Version
 # Publish all packages
 foreach($project in $projects) {
     $projectName = $project.Substring($project.LastIndexOf("/") + 1)
-    & dotnet nuget push ($projectName + "." + $version + ".nupkg") -s https://api.nuget.org/v3/index.json -k "oy2frozbnr422gvnyoeqdmknzikzhkbulkfxqjegz5lk5q"
+    & dotnet nuget push ($projectName + "." + $version + ".nupkg") -s https://api.nuget.org/v3/index.json -k "apikey"
 }
 
 # Go back to the pack folder
