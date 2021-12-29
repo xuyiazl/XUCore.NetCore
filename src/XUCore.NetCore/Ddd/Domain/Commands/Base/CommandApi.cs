34f0923074428402fa84393ecc1a38ad4fe72eac
xuyiazl <3624091@qq.com>
2021-12-29 09:59:33 +0800

    增加CommandApi

---
+++ src/XUCore.NetCore/Ddd/Domain/Commands/Base/CommandApi.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace XUCore.Ddd.Domain
+{
+    /// <summary>
+    /// 抽象命令基类
+    /// </summary>
+    //public abstract class Command<TResponse> : Message, IRequest<TResponse>
+    public abstract class Command<TResponse> : Command, IRequest<TResponse>
+    {
+        ///// <summary>
+        ///// 时间戳
+        ///// </summary>
+        //public DateTime Timestamp { get; private set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        protected Command() : base()
+        {
+            //Timestamp = DateTime.Now;
+        }
+    }
+}
