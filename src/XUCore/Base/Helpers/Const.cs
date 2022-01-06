82af41ff69bef8d54f33d90057dfcf13adcc9b67
xuyiazl <3624091@qq.com>
2022-01-06 10:33:26 +0800

    GetTel(随机生成电话号码)

--- src/XUCore/Base/Helpers/Const.cs
+++ src/XUCore/Base/Helpers/Const.cs
@@ -904,25 +904,5 @@ namespace XUCore.Helpers
         };
 
         #endregion Mime
-
-        /// <summary>
-        /// 手机号码段
-        /// </summary>
-        public static string[] TelStarts = "134,135,136,137,138,139,150,151,152,157,158,159,130,131,132,155,156,133,153,180,181,182,183,185,186,176,187,188,189,177,178".Split(',');
-
-        /// <summary>
-        /// 随机生成电话号码
-        /// </summary>
-        /// <returns></returns>
-        public static string GetRandomTel()
-        {
-            var ran = new Random();//随机数
-            int n = ran.Next(10, 1000);
-            int index = ran.Next(0, TelStarts.Length - 1);
-            string first = TelStarts[index];
-            string second = (ran.Next(100, 888) + 10000).ToString().Substring(1);
-            string thrid = (ran.Next(1, 9100) + 10000).ToString().Substring(1);
-            return first + second + thrid;
-        }
     }
 }
